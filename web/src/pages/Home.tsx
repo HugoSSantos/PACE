6b98848db4d09cca9a93b9e4095090c5f8937265
Hsouzasantos <hugosantos354@gmail.com>
2022-10-24 01:26:26 -0300

    Create the Home and sideBar for the Dashboard page

---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export function Home(){
+    return(
+        <div className="flex h-[100vh]">
+            <div className="w-[40%] h-full bg-white">
+
+            </div>
+            <div className="w-[60%] h-full bg-orange-500">
+
+            </div>
+        </div>
+    )
+}
