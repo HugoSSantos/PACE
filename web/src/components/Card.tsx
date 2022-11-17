3301e6e118b199f68a7fd969c76bf75f9f315a45
HugoSSantos <hss@cesar.school>
2022-11-17 11:44:04 -0300

    Rename files

--- web/src/components/Card.tsx
+++ web/src/components/Card.tsx
@@ -8,7 +8,7 @@ interface cardProps{
 export function Card({title, description, nomeItem}: cardProps){
 
     return(
-        <button className="flex flex-col items-center justify-start w-[70%] h-40 p-4 mx-7 mt-4 border rounded-xl border-black bg-gray-300"
+        <button className="flex flex-col items-center justify-start w-[60%] h-40 p-4 mx-7 mt-4 border rounded-xl border-black bg-gray-300"
             onClick={() => console.log("clicou card")}>
             <h3 className="flex items-center justify-center text-center w-full">{title}</h3>
             <p className="my-3">{description}</p>
